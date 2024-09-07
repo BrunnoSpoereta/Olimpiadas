3b6913846e952fae13565a785995a9bfef148725
Brunno Spoereta  Chiarigatti da Silva <141363674+BrunnoSpoereta@users.noreply.github.com>
2024-09-07 16:35:02 -0300

    Add files via upload

---
+++ dados1.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A Fadinha do Skate! Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Ela já é considerada uma das maiores skatistas de todos os tempos.",
+        link: "https://www.instagram.com/rayssalealsk8/?hl=pt-br",
+        tags:"skate sk8 fada"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Atleta de judô, destaque no cenário nacional e internacional.",
+        link: "https://www.instagram.com/_beatrizsouzaar/",
+        tags: "judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Ginasta olímpica brasileira, campeã mundial e ídolo da nova geração.",
+        link: "https://www.instagram.com/rebecarandrade/?hl=pt-br",
+        tags: "gianastica ginástica ouro"
+    }
+];
